cdab2959a5074fac2e6dfc5bbf469d726e729cc1
Maksym <koldun.maks@gmail.com>
2019-04-13 02:28:51 +0300

    App has been set a database connection and basic middlewares

---
+++ src/config/dbopts.js
@@ -0,0 +1,8 @@
+module.exports = {
+  opts: {
+    auto_reconnect: true,
+    poolSize: 40,
+    useNewUrlParser: true
+  }
+};
+  
---
+++ src/config/env.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: process.env.PORT || 5000,
+  host: process.env.HOST || "http://127.0.0.1",
+  dbname: process.env.DBNAME,
+  uri: process.env.DBURI
+};
+  
